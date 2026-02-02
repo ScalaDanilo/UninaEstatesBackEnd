fc0a2063e14f58a992ba33fe873052dd4835debc
ScalaDanilo <danilo.scala@studenti.unina.it>
2026-02-02 16:50:10 +0100

    aggiunta eliminaUtente

--- backend/src/main/kotlin/com/dieti/backend/repository/ImmobileRepository.kt
+++ backend/src/main/kotlin/com/dieti/backend/repository/ImmobileRepository.kt
@@ -1,10 +1,14 @@
 package com.dieti.backend.repository
 
 import com.dieti.backend.entity.ImmobileEntity
+import com.dieti.backend.entity.UtenteRegistratoEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface ImmobileRepository : JpaRepository<ImmobileEntity, UUID> {
     // Qui in futuro potrai aggiungere filtri, es:
     fun findAllByProprietarioUuid(uuid: UUID): List<ImmobileEntity>
+
+    // Metodo magico di Spring Data: cancella tutti gli immobili dove proprietario == quello passato
+    fun deleteByProprietario(proprietario: UtenteRegistratoEntity)
 }
